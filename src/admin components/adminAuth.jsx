397ec4a262639e16e18b8120018f905553b2e0b9
NejahA <130419583+NejahA@users.noreply.github.com>
2025-08-02 12:28:10 +0000

    fix: Commenter les appels API pour le login et le logout dans authProvider

--- src/admin components/adminAuth.jsx
+++ src/admin components/adminAuth.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 axios.defaults.withCredentials = true; // Allow cookies to be sent
-const API_URL = "http://localhost:10000"; // Update with your backend URL
+// const API_URL = "http://localhost:10000"; // Update with your backend URL
 import {jwtDecode} from "jwt-decode" 
 const authProvider = {
     login: async ({ username, password }) => {
@@ -29,24 +29,24 @@ const authProvider = {
         }
 
         
-        axios.post('http://localhost:10000/api/login', { email: username, password }, { withCredentials: true })
-            .then(async res => {
-              const decodedToken = await jwtDecode(res);
-              await localStorage.setItem('permissions', decodedToken.isAdmin);
-              console.log("is admin =>",decodedToken.isAdmin, " in storage", localStorage.getItem('permissions')); 
-              if (decodedToken.isAdmin==true){
+        // axios.post('http://localhost:10000/api/login', { email: username, password }, { withCredentials: true })
+        //     .then(async res => {
+        //       const decodedToken = await jwtDecode(res);
+        //       await localStorage.setItem('permissions', decodedToken.isAdmin);
+        //       console.log("is admin =>",decodedToken.isAdmin, " in storage", localStorage.getItem('permissions')); 
+        //       if (decodedToken.isAdmin==true){
 
-                return Promise.resolve();
-              }
+        //         return Promise.resolve();
+        //       }
               
-              else {
-                return Promise.reject("You are not an admin");
-              }
-              // localStorage.setItem('token', token);
+        //       else {
+        //         return Promise.reject("You are not an admin");
+        //       }
+        //       // localStorage.setItem('token', token);
               
-            })
-            .catch( err => new Error(err.response.data)
-            )
+        //     })
+        //     .catch( err => new Error(err.response.data)
+        //     )
       },
     
       logout: async () => {
@@ -57,16 +57,16 @@ const authProvider = {
         localStorage.clear();
         return Promise.resolve();
 
-        axios.post('http://localhost:10000/api/logout', {  }, { withCredentials: true })
-            .then(res => {
-              return Promise.resolve();
-            })
-            .catch( err => new Error(err.response.data)
-            )
+        // axios.post('http://localhost:10000/api/logout', {  }, { withCredentials: true })
+        //     .then(res => {
+        //       return Promise.resolve();
+        //     })
+        //     .catch( err => new Error(err.response.data)
+        //     )
       },
       checkError: async (error) => {
         
-      },`${import.meta.env.VITE_GITHUB_URI}/api/users/logged`
+      },
       checkAuth: async () => {
         const response = await fetch(`${import.meta.env.VITE_GITHUB_URI}/api/users/logged`, {
           method: "GET",
