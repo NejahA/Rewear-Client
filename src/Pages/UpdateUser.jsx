531769e09f03ca0aecf2ff03920cc86065f71044
NejahA <130419583+NejahA@users.noreply.github.com>
2025-08-31 00:38:58 +0100

    Add SweetAlert2 import to UpdateUser component for enhanced user feedback

--- src/Pages/UpdateUser.jsx
+++ src/Pages/UpdateUser.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 // import Logout from "../Components/Logout";
 import { useNavigate, useParams } from "react-router-dom";
+import Swal from 'sweetalert2';
 
 const UpdateUser = () => {
   const [user, setUser] = useState({
