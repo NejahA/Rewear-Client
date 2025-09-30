7dabc6303c23466ff7e6ee59396fad41c1ad993d
NejahA <130419583+NejahA@users.noreply.github.com>
2025-09-30 15:19:21 +0100

    fix: Import CameraCapture component in Edit page

--- src/Pages/Edit.jsx
+++ src/Pages/Edit.jsx
@@ -14,6 +14,7 @@ import {
   RadioGroup,
   FormControlLabel,
 } from '@mui/material';
+import CameraCapture from "../Components/CameraCapture ";
 
 const sizeMarks = [
   { value: 0, label: 'XXS' },
